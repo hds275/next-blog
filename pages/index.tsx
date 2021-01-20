ec6b0bfe9aba326e1bd92a7a6972143038ca1808
Andrew <hds.da275@gmail.com>
2021-01-20 10:42:42 +0200

    setup: add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Header from '../components/header'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container">Test</div>
+      </main>
+    </>
+  )
+}
